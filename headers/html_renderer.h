c75c0629a399ff3881f2bc6060daafaebdb28d2a
Anchal <155175388+Anchal-T@users.noreply.github.com>
2025-06-19 17:01:09 +0000

    initial code for html renderer

---
+++ headers/html_renderer.h
@@ -0,0 +1,11 @@
+#ifndef HTML_RENDERER_H
+#define HTML_RENDERER_H
+
+#include <stddef.h>
+
+char *html_renderer(const char *html_content);
+static void render_node(xmlNode *node, char **output, size_t *size, size_t *capacity);
+static void append_to_output(char **output, size_t *size, size_t *capacity, const char *text);
+static char *clean_text(const char *text);
+
+#endif
---
+++ src/html_renderer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libxml/HTMLparser.h>
+#include <libxml/parser.h>
+#include "../headers/html_renderer.h"
+
+#define RESET "\033[0m"
+#define BOLD "\033[1m"
+#define BLUE "\033[34m"
+#define GREEN "\033[32m"
+#define RED "\033[31m"
+#define YELLOW "\033[33m"
+#define CYAN "\033[36m"
+
