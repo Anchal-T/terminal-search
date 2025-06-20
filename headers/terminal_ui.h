023acbbbeec5e09c082d38fac5884cf337861eb1
Anchal <155175388+Anchal-T@users.noreply.github.com>
2025-06-20 15:05:58 +0000

    header for terminal ui

---
+++ headers/terminal_ui.h
@@ -0,0 +1,28 @@
+#ifndef TERMINAL_UI_H
+#define TERMINAL_UI_H
+
+#include <ncurses.h>
+#include <cjson/cJSON.h>
+
+typedef struct{
+    char *title;
+    char *url;
+    char *snippet;
+} SearchResult;
+
+typedef struct{
+    SearchResult *results;
+    int count;
+    int selected;
+    int scroll_offset;
+} SearchState;
+
+void init_ui();
+void cleanup_ui();
+void display_results(SearchState *state, const char *query);
+int handle_input(SearchState *state);
+void open_url(const char *url);
+SearchState* parse_duckduckgo_results(cJSON *json);
+void free_search_state(SearchState *state);
+
+#endif // TERMINAL_UI_H
